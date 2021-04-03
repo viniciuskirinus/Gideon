8756eca68edc3005752a33ef6db83383d0a10ace
viniciuskirinus <vkirinus@gmail.com>
2021-04-02 21:14:03 -0300

    Arquivos do vosk foram adicionados

--- README.md
+++ README.md
@@ -6,6 +6,6 @@ Foco:
     Rápido e expansível.
 
 Técnologias:
-    Reconhecimento de voz. (Google)
+    Reconhecimento de voz. (Google para reconhecimento online, vosk para reconhecimento offline)
     Síntese de voz.
     Algum tipo de IA.
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ model = Model('model')
 rec = KaldiRecognizer(model, 16000)
 
 p = pyaudio.PyAudio()
-stream = p.open(format-pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000)
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000)
 stream.start_stream()
 
 
